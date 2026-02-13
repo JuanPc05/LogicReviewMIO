d1a69e98245044447c5da79e1026fe8a80756305
JuanPc05 <jpcastillor1@gmail.com>
2026-02-12 22:38:23 -0500

    Create a new class, Room, also create getters and behavior

---
+++ src/main/java/com/logicHotel/hotel/model/EstadoHabitacion.java
@@ -0,0 +1,7 @@
+package com.logicHotel.hotel.model;
+
+public enum  EstadoHabitacion{
+    DISPONIBLE,
+    OCUPADA,
+    MANTENIMIENTO
+}
--- src/main/java/com/logicHotel/hotel/model/Habitacion.java
+++ src/main/java/com/logicHotel/hotel/model/Habitacion.java
@@ -1,5 +1,63 @@
 package com.logicHotel.hotel.model;
 
+import java.math.BigDecimal;
+
 public class Habitacion {
     private final int id;
+    private int numero;
+    private String tipo;
+    private BigDecimal precioPorNoche;
+    private EstadoHabitacion estado;
+
+    public Habitacion(int id, int numero, String tipo, BigDecimal precioPorNoche) {
+        this.id = id;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.precioPorNoche = precioPorNoche;
+        this.estado = EstadoHabitacion.DISPONIBLE;
+    }
+
+    /*
+    * GETTERS
+    * */
+    public int getId() {
+        return id;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+    public BigDecimal getPrecioPorNoche() {
+        return precioPorNoche;
+    }
+
+    public EstadoHabitacion getEstado() {
+        return estado;
+    }
+
+    /*
+    * COMPORTAMIENTO
+    * */
+
+    public void ocupar() {
+        if (estado != EstadoHabitacion.DISPONIBLE) {
+            throw new IllegalStateException("La habitación no está disponible");
+        }
+        estado = EstadoHabitacion.OCUPADA;
+    }
+
+    public void enviarMantenimiento(){
+        estado = EstadoHabitacion.MANTENIMIENTO;
+    }
+
+    public void actualizarPrecio(BigDecimal nuevoPrecio){
+        if(nuevoPrecio.compareTo(precioPorNoche) <= 0){
+            throw new IllegalArgumentException("El precio debe ser mayor a 0.");
+        }
+        this.precioPorNoche = nuevoPrecio;
+    }
+
 }
